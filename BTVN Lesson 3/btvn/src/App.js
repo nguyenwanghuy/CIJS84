3698ad5a4096e874f2ecee8cfd4977861f2296df
nguyenwanghuy <nguyenquanghuy04012001@gmail.com>
2023-05-18 23:22:14 +0700

    btvn3

---
+++ BTVN Lesson 3/btvn/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Navi from './Layout/Navi';
+import Header from './Layout/Header';
+import Footer from './Layout/Footer';
+import Content from './Content';
+function App() {
+  return (
+  <>
+  <Navi/>
+  <Header/>
+  <Content/>
+  <Footer/>
+
+  </>
+   
+  );
+}
+
+export default App;
